990a704a8e07cbf3e411ac286145194d05c8b06f
loulouexe <141170753+louloupexe@users.noreply.github.com>
2025-03-22 12:20:25 +0100

    Update main.ps1

--- GIF-Player/main.ps1
+++ GIF-Player/main.ps1
@@ -8,7 +8,7 @@ $gifPath = "$env:temp/g.gif"
 iwr -Uri $url -OutFile $gifPath
 $ErrorActionPreference = 'Stop'
 
-# Création de l'icône pour le Tray
+# Création de l'icône pour le System Tray
 $trayIcon = New-Object System.Windows.Forms.NotifyIcon
 $trayIcon.Icon = [System.Drawing.SystemIcons]::Information
 $trayIcon.Visible = $true
@@ -20,54 +20,65 @@ $exitItem = New-Object System.Windows.Forms.MenuItem "Quitter"
 $menu.MenuItems.Add($exitItem)
 $trayIcon.ContextMenu = $menu
 
-# Fonction pour afficher le GIF
+# Variable pour stocker le processus du GIF
+$script:gifProcess = $null
+
+# Fonction pour afficher le GIF en boucle et empêcher sa fermeture
 function Show-Gif {
-    $form = New-Object System.Windows.Forms.Form
-    $pictureBox = New-Object System.Windows.Forms.PictureBox
-    $timer = New-Object System.Windows.Forms.Timer
+    while ($true) {
+        $form = New-Object System.Windows.Forms.Form
+        $pictureBox = New-Object System.Windows.Forms.PictureBox
+        $timer = New-Object System.Windows.Forms.Timer
 
-    $form.FormBorderStyle = 'None'
-    $form.ShowInTaskbar = $false
-    $form.BackColor = [System.Drawing.Color]::Black
-    $form.Size = New-Object System.Drawing.Size(490, 300)
-    $form.StartPosition = 'CenterScreen'
-    $form.TopMost = $true
+        $form.FormBorderStyle = 'None'    # Pas de bordure
+        $form.ShowInTaskbar = $false      # Ne pas afficher dans la barre des tâches
+        $form.BackColor = [System.Drawing.Color]::Black
+        $form.Size = New-Object System.Drawing.Size(490, 300)
+        $form.StartPosition = 'CenterScreen'
+        $form.TopMost = $true             # Toujours au premier plan
 
-    $pictureBox.Size = $form.Size
-    $pictureBox.Image = [System.Drawing.Image]::FromFile($gifPath)
+        # Empêcher la fermeture avec ALT+F4
+        $form.Add_FormClosing({ $_.Cancel = $true })
 
-    # Animation du GIF
-    $timer.Interval = 50
-    $timer.Add_Tick({
-        $pictureBox.Image.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Time, $timer.Tag)
-        $pictureBox.Refresh()
-        $timer.Tag = ($timer.Tag + 1) % $pictureBox.Image.GetFrameCount([System.Drawing.Imaging.FrameDimension]::Time)
-    })
-    $timer.Tag = 0
+        $pictureBox.Size = $form.Size
+        $pictureBox.Image = [System.Drawing.Image]::FromFile($gifPath)
 
-    $form.Controls.Add($pictureBox)
-    $form.Add_Shown({ $timer.Start() })
-    
-    # Fermer après 5 secondes
-    Start-Sleep -Seconds 5
-    $form.Close()
-    $form.Dispose()
+        # Animation du GIF
+        $timer.Interval = 50
+        $timer.Add_Tick({
+            $pictureBox.Image.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Time, $timer.Tag)
+            $pictureBox.Refresh()
+            $timer.Tag = ($timer.Tag + 1) % $pictureBox.Image.GetFrameCount([System.Drawing.Imaging.FrameDimension]::Time)
+        })
+        $timer.Tag = 0
+
+        $form.Controls.Add($pictureBox)
+        $form.Add_Shown({ $timer.Start() })
+        $script:gifProcess = $form
+
+        # Affiche le GIF (bloquant)
+        $form.ShowDialog()
+    }
 }
 
 # Clic gauche sur l'icône pour afficher le GIF
 $trayIcon.Add_MouseClick({
     param($sender, $e)
     if ($e.Button -eq [System.Windows.Forms.MouseButtons]::Left) {
-        Show-Gif
+        Start-Job -ScriptBlock { Show-Gif }
     }
 })
 
 # Clic droit sur "Quitter" pour fermer
 $exitItem.Add_Click({
     $trayIcon.Dispose()
+    Stop-Job -Name "Show-Gif" -ErrorAction SilentlyContinue
     Remove-Item $gifPath -ErrorAction SilentlyContinue
     exit
 })
+
+# Lancer le GIF immédiatement
+Start-Job -ScriptBlock { Show-Gif }
 
 # Exécution en arrière-plan
 [System.Windows.Forms.Application]::Run()
